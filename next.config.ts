83e98b476610fdee9142e376ac64922ba7ff227f
Pedro Yuris Machado Leiva <ptermach01@gmail.com>
2025-10-26 20:52:14 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'azure-petite-swan-479.mypinata.cloud',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
